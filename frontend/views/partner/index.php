97e76e830cf0b063d2bf46aeadf41d5956113e33
Bipin Joshi <joshibipin2052@gmail.com>
2020-02-28 13:55:59 +0545

    few more changes in partner ko single page

--- frontend/views/partner/index.php
+++ frontend/views/partner/index.php
@@ -2,34 +2,58 @@
     /* @var $this yii\web\View */
     $this->title = 'Partner';
 ?>
-<div class = "page-header">
-    <div class = "container">
-        <h1 class = "title"><?php echo $client['title']; ?></h1>
-    </div>
+   <?php if(isset($client)) {?>
+    <div class = "page-header">
+        <div class = "container">
+            <h1 class = "title"><?php echo $client['title']; ?></h1>
+        </div></div>
 
-</div><!-- page-header -->
-<section class = "page-section animated fadeInUp visible" data-animation = "fadeInUp">
-    <div class = "container">
+        <section class = "page-section animated fadeInUp visible" data-animation = "fadeInUp">
+            <div class = "container">
 
-        <div class = "row">
-            <div class = "col-md-3">
-                <!-- Image -->
-                <img alt = "" src = "<?php echo Yii::$app->request->baseUrl . '/common/assets/images/uploads/' . $client['client']['image']; ?>">
+                <div class = "row">
+                    <div class = "col-md-3">
+                        <!-- Image -->
+                        <img alt = "" src = "<?php echo Yii::$app->request->baseUrl . '/common/assets/images/uploads/' . $client['client']['image']; ?>">
+                    </div>
+                    <div class = "col-md-9">
+                        <div class = "section-title text-left animated fadeInUp visible" data-animation = "fadeInUp">
+                            <!-- Title -->
+                            <h2 class = "title"><?php echo $client['title']; ?></h2>
+                        </div>
+                        <!-- Content -->
+                        <div data-animation = "fadeInDown" class = "animated fadeInDown visible">
+                            <p>URL: <a href = "<?php echo $client['client']['link']; ?>"><?php echo $client['client']['link']; ?></a></p>
+                        </div>
+                        <div data-animation = "fadeInDown" class = "animated fadeInDown visible">
+                            <p><?php echo $client['content']; ?>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class = "col-md-9">
-                <div class = "section-title text-left animated fadeInUp visible" data-animation = "fadeInUp">
-                    <!-- Title -->
-                    <h2 class = "title"><?php echo $client['title']; ?></h2>
-                </div>
-                <!-- Content -->
-                <div data-animation = "fadeInDown" class = "animated fadeInDown visible">
-                    <p>URL: <a href = "<?php echo $client['client']['link']; ?>"><?php echo $client['client']['link']; ?></a></p>
-                </div>
-                <div data-animation = "fadeInDown" class = "animated fadeInDown visible">
-                    <p><?php echo $client['content']; ?>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+        </section>
+
+   <?php }else{?>
+       <div class = "page-header">
+           <div class = "container">
+               <h1 class = "title">No Information Found</h1>
+           </div>
+       </div>
+           <section class = "page-section animated fadeInUp visible" data-animation = "fadeInUp">
+               <div class = "container">
+                   <div class = "row">
+                       <div class = "col-md-12">
+                           <div class = "section-title text-left animated fadeInUp visible" data-animation = "fadeInUp">
+                               <!-- Title -->
+                               <h2 class = "title">No Information Available</h2>
+                           </div>
+                           <!-- Content -->
+                       </div>
+                   </div>
+               </div>
+           </section>
+
+   <?php } ?>
+
+
